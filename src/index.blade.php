cffc23dea2138efa611ee7b7fadb20326b410b4c
boxshadow <idafoo@sina.com>
2017-03-06 22:28:18 +0800

    merge

--- src/index.blade.php
+++ src/index.blade.php
@@ -2,13 +2,8 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-<<<<<<< HEAD
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-=======
-  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-  <meta name="format-detection" content="telephone=no">
->>>>>>> ae33f7bf2a6c9ab2082bd7cdd9cdd5db886274fd
   <title>Demo</title>
 </head>
 <body>
