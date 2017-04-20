83fe0088015f17ccc17a39af38ec9dc8f4795a94
Seven <shiweidu@outlook.com>
2017-04-20 13:55:09 +0800

    完成全新构建。

--- php/helper.php
+++ php/helper.php
@@ -2,6 +2,8 @@
 
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentWeb;
 
+use function mix as globalMix;
+
 /**
  * Get the evaluated view contents for the given view.
  *
@@ -27,8 +29,51 @@ function view($view = null, $data = [], $mergeData = [])
     return $factory->make($view, $data, $mergeData);
 }
 
+/**
+ * Get the component base path.
+ *
+ * @param string $path
+ * @return string
+ * @author Seven Du <shiweidu@outlook.com>
+ * @homepage http://medz.cn
+ */
 function base_path($path = '')
 {
-    return dirname(__DIR__).$path;
+    if (! $path) {
+        return dirname(__DIR__);
+    }
+
+    return dirname(__DIR__).'/'.$path;
+}
+
+/**
+ * Get the path to a versioned Mix file.
+ *
+ * @param string $path
+ * @return \Illuminate\Support\HtmlString
+ * @author Seven Du <shiweidu@outlook.com>
+ */
+function mix($path)
+{
+    return globalMix($path, component_name());
+}
+
+/**
+ * Get the component name.
+ *
+ * @return string
+ * @author Seven Du <shiweidu@outlook.com>
+ */
+function component_name(): string
+{
+    static $name;
+
+    if (! $name) {
+        $name = json_decode(
+            app(\Illuminate\Filesystem\Filesystem::class)->get(base_path('composer.json'))
+        )->name;
+    }
+
+    return $name;
 }
 
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,22 @@
-var webpack = require('webpack');
-var path = require('path');
-var ExtractTextPlugin = ('extract-text-webpack-plugin');
-// import autoprefixer from 'autoprefixer';
-// import { StatsWriterPlugin } from 'webpack-stats-plugin';
-// import lodash from 'lodash';
-// import fs from 'fs';
+/*
+|--------------------------------------------------------
+| ThinkSNS+ SPA 配置文件
+|--------------------------------------------------------
+|
+| 配置文件使用 ES6 语法配置，这样能保证整个文档项目的语法统一性
+| 修改配置文件请使用 ES6 语法对 webpack 进行配置。
+|
+| @author Seven Du <shiweidu@outlook.com>
+|
+*/
+
+import webpack from 'webpack';
+import path from 'path';
+import fs from 'fs';
+
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import WebpackLaravelMixManifest from 'webpack-laravel-mix-manifest';
+import OptimizeCSSPlugin from 'optimize-css-assets-webpack-plugin';
 
 // 环境变量获取
 const NODE_ENV = process.env.NODE_ENV || 'development';
@@ -14,64 +26,134 @@ const isHot = process.argv.includes('--hot');
 const isProd = NODE_ENV === 'production';
 
 // 各项资源地址定义
-const assetsRoot = path.join(__dirname, 'asset');
-const buildAssetsRoot = path.resolve(__dirname, 'dist/');
+const assetsRoot = path.resolve(__dirname, /*'asset'*/ 'asset');
+const buildAssetsRoot = path.join(__dirname, 'dist');
 
 // 入口配置
 const entry = {
-  user: path.join(assetsRoot, 'index.js')
+  app: path.resolve(assetsRoot, 'index.js')
 };
 
+function cssLoaders (options = {}) {
+  const cssLoader = {
+    loader: 'css-loader',
+    options: {
+      minimize: isProd,
+      sourceMap: options.sourceMap
+    }
+  };
+
+  // generate loader string to be used with extract text plugin
+  function generateLoaders (loader, loaderOptions) {
+    let loaders = [cssLoader];
+    if (loader) {
+      loaders.push({
+        loader: loader + '-loader',
+        options: Object.assign({}, loaderOptions, {
+          sourceMap: options.sourceMap
+        })
+      });
+    }
+
+    return ExtractTextPlugin.extract({
+      use: loaders,
+      fallback: 'vue-style-loader'
+    });
+  }
+
+  // https://vue-loader.vuejs.org/en/configurations/extract-css.html
+  return {
+    css: generateLoaders(),
+    postcss: generateLoaders(),
+    less: generateLoaders('less'),
+    sass: generateLoaders('sass', { indentedSyntax: true }),
+    scss: generateLoaders('sass'),
+    // stylus: generateLoaders('stylus'),
+    // styl: generateLoaders('stylus')
+  };
+};
+
+function styleLoaders(options = {}) {
+  let output = [];
+  const loaders = cssLoaders(options);
+
+  for (let extension in loaders) {
+    let loader = loaders[extension]
+    output.push({
+      test: new RegExp('\\.' + extension + '$'),
+      use: loader
+    });
+  }
+
+  return output;
+};
+
+// 环境插件～不同环境启用的不同插件.
+const plugins = isProd ?
+[
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    sourceMap: false
+  }),
+  // Compress extracted CSS. We are using this plugin so that possible
+  // duplicated CSS from different components can be deduped.
+  new OptimizeCSSPlugin({
+    cssProcessorOptions: {
+      safe: true
+    }
+  })
+] : 
+[
+  // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+  new webpack.NoEmitOnErrorsPlugin(),
+  ...(isHot ? [
+    new webpack.HotModuleReplacementPlugin(),
+  ] : [])
+];
+
 const webpackConfig = {
-  entry,
+  devtool: isProd ? false : 'source-map',
+  entry: entry,
   output: {
-    path: path.join(buildAssetsRoot),
-    // filename: isProd ? 'js/[name].[chunkhash].js' : 'js/[name].js',
-    filename: 'js/[name].js'
+    path: isHot ? '/' : buildAssetsRoot,
+    publicPath: isHot ? 'http://localhost:8080/' : '../',
+    filename: isProd ? 'js/[chunkhash].js' : 'js/[name].js',
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     modules: [
       assetsRoot,
-      path.join(__dirname, 'node_modules')
+      path.join(__dirname, 'node_modules'),
     ],
     alias: {
-      'jquery': `jquery/dist/jquery.${isProd ? 'min.js' : 'js'}`,
-      'vue$': 'vue/dist/vue.common.js'
+      'vue$': `vue/dist/vue.common.js`,
     }
   },
   module: {
     rules: [
-      // {
-      //   test: /\.(js|vue)$/,
-      //   loader: 'eslint-loader',
-      //   enforce: "pre",
-      //   include: [
-      //     assetsRoot,
-      //   ]
-      // },
+      ...styleLoaders({ sourceMap: !isProd }),
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {
-            scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
-          }
+          loaders: cssLoaders({
+            sourceMap: !isProd
+          })
         }
       },
       {
-        test: /\.(scss|css)$/,
-        loader: 'style-loader!css-loader!postcss-loader!sass-loader',
-        query: {
-          name: 'css/[name].css'
-        }
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [assetsRoot]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         query: {
           limit: 10000,
-          name: isProd ? 'images/[name].[ext]' : `images/[name].[ext]`
+          name: isProd ? 'images/[hash].[ext]' : `images/[name].[ext]`
         }
       },
       {
@@ -79,18 +161,37 @@ const webpackConfig = {
         loader: 'url-loader',
         query: {
           limit: 10000,
-          name: isProd ? 'fonts/[name].[ext]' : `fonts/[name].[ext]`
+          name: isProd ? 'fonts/[hash].[ext]' : `fonts/[name].[ext]`
         }
       }
     ]
-  }
-  ,
+  },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(NODE_ENV),
+      },
+    }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: isProd ? 'css/[name].css' : 'css/[name].css'
-    })
+      filename: isProd ? 'css/[chunkhash].css' : 'css/[name].css'
+    }),
+    new WebpackLaravelMixManifest(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    ...plugins,
   ]
 };
 
-// export default webpackConfig;
+// mix.
+if (isHot) {
+
+  // hot file.
+  const hotFile = path.join(buildAssetsRoot, 'hot');
+  if (fs.existsSync(hotFile)) {
+    fs.unlinkSync(hotFile);
+  }
+  // hot reloading enabled
+  fs.writeFileSync(hotFile, 'hot reloading enabled');
+}
+
+export default webpackConfig;
