de8e3cc5c3707384f0b69a76a1d8fa691bafd2aa
boxshadow <idafoo@sina.com>
2017-06-27 15:51:26 +0800

    optimize im

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -326,9 +326,9 @@
                       }
                     })
                   })
-                  .catch(e => {
-                    console.log(e);
-                  });
+                })
+                .catch(e => {
+                   console.log(e);
                 });
               }
             });
