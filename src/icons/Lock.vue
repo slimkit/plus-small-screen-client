489bd7ce566837a1425e86a2bdf9fe510b2c7d37
boxshadow <idafoo@sina.com>
2017-07-19 01:43:44 +0800

    fixed

--- src/icons/Lock.vue
+++ src/icons/Lock.vue
@@ -1,5 +1,5 @@
 <template>
-  	<svg viewBox="0 0 24 24" :height="height" :width="width" style="display: inherit;">
+  	<svg viewBox="0 0 24 24" :height="height" :width="width" :fill="color" style="display: inherit;">
     	<g>
 			<path class="st0" d="M19.2,10.3h-1.6V6.6C17.6,3.5,15,1,12,1C9,1,6.4,3.5,6.4,6.6v3.7H4.8c-0.7,0-1.4,0.6-1.4,1.4v10
 			c0,0.7,0.6,1.4,1.4,1.4h14.4c0.7,0,1.4-0.6,1.4-1.4v-10C20.6,10.8,19.9,10.3,19.2,10.3L19.2,10.3z M12.7,16.9v2.2
@@ -19,6 +19,10 @@ const Lock = {
     width: {
       type: [String, Number],
       default: 24
+    },
+    color: {
+      type: String,
+      default: '#fff'
     }
   }
 }
