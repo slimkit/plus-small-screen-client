65a866f4039de8ee7d1c7a7fb11ae9ab6b61ddd2
boxshadow <idafoo@sina.com>
2017-07-19 18:11:21 +0800

    update

--- src/components/FeedImages.vue
+++ src/components/FeedImages.vue
@@ -1,6 +1,6 @@
 <template>
   <section style="width: 68vw; position: relative; margin-bottom: 8px; display: inherit">
-    <img :style="`width: 100%; height: ${imageHeight}; object-fit: cover; max-height: 80vw;`" v-if="imageSize == 1 && imageObject[0].paid" @click.native="showSwiper(0)"  v-lazy="imageObject[0].url" />
+    <img :style="`width: 100%; height: ${imageHeight}; object-fit: cover; max-height: 80vw;`" v-if="imageSize == 1 && imageObject[0].paid" @click.native.stop ="showSwiper(0)"  v-lazy="imageObject[0].url" />
     <LockedImage :single="true" v-if="imageSize==1 && !imageObject[0].paid" @click.stop="showSwiper(0)"/>
     <div style="width: 100%; display: flex;" v-if="imageSize == 2">
       <div style="width: 34vw;" :class="$style.showImgBox">
