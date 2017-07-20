26d06a37b8686ab64d6d98a7d8fa04dd421311ea
boxshadow <idafoo@sina.com>
2017-07-20 14:18:47 +0800

    兼容路由

--- php/routes.php
+++ php/routes.php
@@ -8,3 +8,11 @@ Route::namespace('Zhiyi\\Component\\ZhiyiPlus\\PlusComponentH5\\Controllers')
         	->where('route', '.*')
             ->name('H5');
     });
+Route::namespace('Zhiyi\\Component\\ZhiyiPlus\\PlusComponentH5\\Controllers')
+    ->prefix('/h5')
+    // ->middleware('h5')
+    ->group(function () {
+        Route::get('/{route?}', 'HomeController@index')
+        	->where('route', '.*')
+            ->name('H5');
+    });
