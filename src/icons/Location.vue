6abe4ff33b053443e44b2d4747b50fb4ace662a0
jsonleex <jsonleex@163.com>
2017-08-25 17:19:26 +0800

    找人 - 通讯录ICON, 找人 - 定位ICON

---
+++ src/icons/Location.vue
@@ -0,0 +1,46 @@
+<template>
+    <svg viewBox="0 0 24 24" :height="height" :width="width">
+        <path class="st5" d="M12,1C7.5,1,3.8,5.1,5,9.7C6.3,15,12,20.8,12,20.8s5.7-5.8,7-11.1C20.2,5.1,16.5,1,12,1L12,1z M18.1,9.5
+    c-1.2,4.6-6.1,9.9-6.1,9.9S7,14,5.9,9.4C4.9,5.4,8.1,2,12,2C15.8,2,19.1,5.4,18.1,9.5L18.1,9.5z M18.1,9.5" />
+        <path class="st5" d="M12,4.7c-2.2,0-4,1.8-4,4c0,2.2,1.8,4,4,4s4-1.8,4-4C16,6.5,14.2,4.7,12,4.7L12,4.7z M12,11.8
+    c-1.7,0-3.1-1.4-3.1-3.1c0-1.7,1.4-3.1,3.1-3.1c1.7,0,3.1,1.4,3.1,3.1C15.1,10.4,13.7,11.8,12,11.8L12,11.8z M12,11.8" />
+        <path class="st5" d="M15.4,18.5c1.7,0.3,2.8,0.9,2.8,1.6c0,1-2.8,1.9-6.2,1.9c-3.4,0-6.2-0.8-6.2-1.9c0-0.7,1.1-1.2,2.8-1.6
+    l-0.2-0.9c-2.1,0.5-3.5,1.4-3.5,2.5c0,1.6,3.2,2.8,7.1,2.8c3.9,0,7.1-1.3,7.1-2.8c0-1-1.4-2-3.5-2.5L15.4,18.5z M15.4,18.5" />
+    </svg>
+</template>
+<script>
+const Location = {
+    props: {
+        height: {
+            type: [String, Number],
+            default: 24
+        },
+        width: {
+            type: [String, Number],
+            default: 24
+        }
+    }
+}
+export default Location;
+</script>
+
+<style scoped>
+    .st0{fill:#EFB946;}
+    .st1{fill-rule:evenodd;clip-rule:evenodd;fill:#FFFFFF;}
+    .st2{fill-rule:evenodd;clip-rule:evenodd;fill:#333333;}
+    .st3{fill-rule:evenodd;clip-rule:evenodd;}
+    .st4{fill-rule:evenodd;clip-rule:evenodd;fill:#59B6D7;}
+    .st5{fill:#333333;}
+    .st6{fill:#59B6D7;}
+    .st7{fill:#58ADCC;}
+    .st8{fill:#999999;}
+    .st9{fill:#B2B2B2;}
+    .st10{fill-rule:evenodd;clip-rule:evenodd;fill:#FF9400;}
+    .st11{fill:#FFFFFF;}
+    .st12{fill:#BABABA;}
+    .st13{fill-rule:evenodd;clip-rule:evenodd;fill:#BABABA;}
+    .st14{fill:#969EA2;}
+    .st15{fill:none;stroke:#000000;stroke-width:4;stroke-linecap:round;stroke-miterlimit:10;}
+    .st16{fill:#666666;}
+    .st17{fill-rule:evenodd;clip-rule:evenodd;fill:#999999;}
+</style>
