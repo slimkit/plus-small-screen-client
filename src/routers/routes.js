0c798c371181776b39ce40297dd5045101884fb0
huhao <915664508@qq.com>
2017-10-31 18:23:04 +0800

    个人中心问答路由
    个人中心问答路由

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -77,6 +77,19 @@ import Rank from '../views/rank/Rank';
 import RankList from '../views/rank/RankList';
 import RankShow from '../views/rank/RankShow';
 
+
+// 我的问答
+import MyQuestionAnswerMain from '../views/my/questionAnswer/Main';
+import MyQuestionAnswerQuestion from '../views/my/questionAnswer/Question';
+import MyQuestionAnswerQuestionList from '../views/my/questionAnswer/QuestionList.vue';
+
+import MyQuestionAnswerAnswer from '../views/my/questionAnswer/Answer';
+import MyQuestionAnswerAnswerList from '../views/my/questionAnswer/AnswerList';
+
+import MyQuestionAnswerFollow from '../views/my/questionAnswer/Follow';
+import MyQuestionAnswerFollowList from '../views/my/questionAnswer/FollowList';
+
+
 import { requestAuth, CanNotGetInWhenLogged } from '../utils/auth';
 
 const routes = [
@@ -305,8 +318,75 @@ const routes = [
     beforeEnter: (to, from, next) => {
       requestAuth(to, from, next)
     }
-  },
-  {
+  },{
+    path: '/users/question-answer',
+    component: MyQuestionAnswerMain,
+    name: 'myQuestionAnswer',
+    meta: {
+      title: '我的问答'
+    },
+    redirect: '/users/question-answer/questions',
+    children: [
+      {
+        path: '/users/question-answer/questions',
+        component: MyQuestionAnswerQuestion,
+        name: 'myQuestionAnswerQuestion',
+        meta: {
+          title: '我的提问'
+        },
+        redirect: '/users/question-answer/questions/all',
+        children: [
+          {
+            path: '/users/question-answer/questions/:type',
+            component: MyQuestionAnswerQuestionList,
+            name: 'myQuestionAnswerQuestionList',
+            meta: {
+              title: '我的提问'
+            },
+          },
+        ]
+      },{
+          path: '/users/question-answer/answers',
+          component: MyQuestionAnswerAnswer,
+          name: 'myQuestionAnswerAnswer',
+          meta: {
+            title: '我的回答'
+          },
+          redirect: '/users/question-answer/answers/all',
+          children: [
+            {
+              path: '/users/question-answer/answers/:type',
+              component: MyQuestionAnswerAnswerList,
+              name: 'myQuestionAnswerAnswerList',
+              meta: {
+                title: '我的回答'
+              },
+            },
+          ]
+      },{
+          path: '/users/question-answer/follows',
+          component: MyQuestionAnswerFollow,
+          name: 'myQuestionAnswerFollow',
+          meta: {
+            title: '我的关注'
+          },
+          redirect: '/users/question-answer/follows/questions',
+          children: [
+            {
+              path: '/users/question-answer/follows/:type',
+              component: MyQuestionAnswerFollowList,
+              name: 'myQuestionAnswerFollowList',
+              meta: {
+                title: '我的关注'
+              },
+            },
+          ]
+      },
+    ],
+    beforeEnter: (to, from, next) => {
+      requestAuth(to, from, next)
+    }
+  },{
     path: '/users/profile',
     component: Profile,
     name: 'myProfile',
