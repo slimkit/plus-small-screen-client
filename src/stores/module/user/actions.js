1a3f8f7b76e4ccb3b7d084149f2566e6fe58b7b6
JsonLeex <JsonLeex@163.com>
2017-11-06 17:06:48 +0800

    [mod] 改了个错

--- src/stores/module/user/actions.js
+++ src/stores/module/user/actions.js
@@ -12,14 +12,10 @@ export default {
     UPDATE_INFO_OF_MINE({ commit, dispatch, getters, state }, mine) {
         const old = state.mine;
         try {
-            if(_.isEmpty(old)) {
-                localEvent.set('mine', mine);
-            } else {
-                const id = mine.id;
-                if(id === old.id) {
-                    state.mine = Object.assign({}, old, mine);
-                    localEvent.set('mine', state.mine);
-                }
+            const id = mine.id;
+            if(_.isEmpty(old) || id === old.id) {
+                state.mine = Object.assign({}, old, mine);
+                localEvent.set('mine', state.mine);
             }
             commit('ADD_USER_TO_VUEX', [mine]);
         } catch(e) {
@@ -30,16 +26,15 @@ export default {
         dispatch('UPDATE_INFO_OF_MINE', {});
     },
     GET_USER_BY_ID({ commit, dispatch, getters, state }, id) {
-        let rs = state.users[`user_${id}`];
+        let rs = getters.getUserById(id);
         if(!rs) {
-            return dispatch('GET_USER_BY_API', id);
+            dispatch('GET_USER_BY_API', id);
         }
         return rs;
     },
     GET_USER_BY_API({ commit, dispatch, getters, state }, id) {
         getUserByApi(id, (data) => {
             commit('ADD_USER_TO_VUEX', data);
-            // dispatch('GET_USER_BY_ID', id);
         }, (err) => {
             console.log(err);
         });
