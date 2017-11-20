9e0e53f7855c87fc3eed87d46ae2e9782a003998
boxshadow <idafoo@sina.com>
2017-11-20 14:38:52 +0800

    模板增加静态文件版本号

--- views/index.blade.php
+++ views/index.blade.php
@@ -6,8 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <title>ThinkSNS+</title>
   <script src="https://webapi.amap.com/maps?v=1.3&key=abb777bcccf278aa589076944112267e"></script>
-  <link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
-  <link rel="stylesheet" href="{{ mix('css/app.css', 'assets/h5') }}" />
+  <link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.css?v=20171120" rel="stylesheet">
+  <link rel="stylesheet" href="{{ mix('css/app.css', 'assets/h5') }}?v=20171120" />
   <style>
     /*********************** markdown 样式重构 *******************/
     .editor-preview img {
@@ -189,6 +189,6 @@
     }
   </script>
 
-  <script src="{{ mix('js/app.js', 'assets/h5') }}"></script>
+  <script src="{{ mix('js/app.js', 'assets/h5') }}?v=20171120"></script>
 </body>
 </html>
