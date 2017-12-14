b06b266bd2f8ffdf0867f893406f1aa00c7d0f44
JsonLeex <jsonleex@163.com>
2017-12-14 10:16:17 +0800

    [mod] 修改 `登录页面`

--- src_v2/page/signin.vue
+++ src_v2/page/signin.vue
@@ -1,45 +1,51 @@
 <template>
-    <div class="signin_page">
-        <head-top append='true' title='登录'></head-top>
-        <div class="normal_signin">
-            <div class="forms">
-                <div class="forms_row">
-                    <label for="account">账户</label>
-                    <input type="text" v-model='account' id="account" placeholder="用户名/手机号/邮箱">
-                    <v-icon type='base-clean' v-if='account.length > 0' @click.native='account=""' />
-                </div>
-                <div class="forms_row">
-                    <label for="password">密码</label>
-                    <input type="text" v-model='password' id="password" v-if='eye' placeholder="输入6位以上登录密码">
-                    <input type="password" v-model='password' id="password" v-else placeholder="输入6位以上登录密码">
-                    <v-icon :type='`eye-${eye?"open":"close"}`' width='.3rem' height='.3rem' @click.native='eye=!eye' />
-                </div>
-            </div>
-            <div class="forms_error ellipsis">{{ error }}</div>
-            <button class="long_btn" :disabled='disabled' @click='signIn'>登录</button>
-            <div class="other_link">
-                <router-link to='/feed/new'>不登录, 先随便逛逛</router-link>
-                <router-link to='/forgot'>忘记密码</router-link>
-            </div>
-        </div>
-        <!-- 三方登录 -->
-        <div class="tr_signin">
-            <h3>社交账号登录</h3>
-            <div class="tr_signin_group">
-                <figure v-for='(tr, index) in tr_signin' :key='`tr-signin-${index}`'>
-                    <div class="tr_signin_icon">
-                        <v-icon :type='tr.icon'></v-icon>
+    <transition name='router-fadeInRight' mode="out-in">
+        <div class="signin_page">
+            <head-top append='true' title='登录'></head-top>
+            <div class="normal_signin">
+                <div class="forms">
+                    <div class="forms_row">
+                        <label for="account">账户</label>
+                        <input type="text" v-model='account' id="account" placeholder="用户名/手机号/邮箱">
+                        <v-icon type='base-clean' v-if='account.length > 0' @click.native='account=""' />
                     </div>
-                    <p>{{ tr.title }}</p>
-                </figure>
+                    <div class="forms_row">
+                        <label for="password">密码</label>
+                        <input type="text" v-model='password' minlength="6" maxlength="15" id="password" v-if='eye' placeholder="输入6位以上登录密码">
+                        <input type="password" v-model='password' minlength="6" maxlength="15" id="password" v-else placeholder="输入6位以上登录密码">
+                        <v-icon :type='`eye-${eye?"open":"close"}`' @click.native='eye=!eye' />
+                    </div>
+                </div>
+                <div class="forms_error ellipsis">{{ error }}</div>
+                <button class="long_btn" :disabled='disabled' @click='signIn'>
+                    <div>
+                        <span>登录</span>
+                        <v-icon v-show='btnLoading' class='btn-loading' type='base-loading'></v-icon>
+                    </div>
+                </button>
+                <div class="other_link">
+                    <router-link to='/feed/new'>不登录, 先随便逛逛</router-link>
+                    <router-link to='/forgot'>忘记密码</router-link>
+                </div>
+            </div>
+            <!-- 三方登录 -->
+            <div class="tr_signin" v-show='false'>
+                <h3>社交账号登录</h3>
+                <div class="tr_signin_group">
+                    <figure v-for='(tr, index) in tr_signin' :key='`tr-signin-${index}`'>
+                        <div class="tr_signin_icon">
+                            <v-icon :type='tr.icon'></v-icon>
+                        </div>
+                        <p>{{ tr.title }}</p>
+                    </figure>
+                </div>
             </div>
         </div>
-    </div>
+    </transition>
 </template>
 <script>
 import HeadTop from '../components/HeadTop';
-import { detectOS } from '../util/';
-
+import validate, { addValidate } from '../util/validate';
 export default {
     name: 'signin',
     components: {
@@ -50,7 +56,8 @@ export default {
             account: '',
             password: '',
             eye: false,
-            error: '账号或密码错误',
+            error: '',
+            btnLoading: false,
 
             /* 后台获取三方登录数据 */
             tr_signin: [{
@@ -67,32 +74,58 @@ export default {
     },
     computed: {
         disabled() {
-            return(this.account.length < 2 || this.password.length < 6);
+            return(this.account.length < 2 || this.password.length < 6 || this.btnLoading);
         }
     },
     methods: {
         signIn() {
-            let device_code = detectOS(),
-                param = {
-                    login: this.account,
-                    password: this.password,
-                    device_code
-                };
-            window.alert(device_code);
-            this.$http.post('tokens', param).then(res => {
-                console.log(res);
-            })
+            let count = 0;
+            this.error = '';
+            this.btnLoading = true;
+            // 需要验证的 数据
+            const vals = [{
+                val: this.account,
+                type: 'account'
+            }, {
+                val: this.password,
+                type: 'password'
+            }];
+
+            vals.map(v => validate(v)).forEach(({ r, tips }) => {
+                r ? count++ : this.error = tips;
+            });
+
+            if(count === vals.length) {
+                let device_code = this.$store.state.BROWSER.OS,
+                    param = {
+                        login: this.account,
+                        password: this.password,
+                        device_code
+                    };
+
+                /* 用户登录 */
+                this.$http.post('tokens', param, {
+                    validateStatus: s => s === 201
+                }).then(({ data }) => {
+                    this.btnLoading = false;
+                    const { token, user } = data;
+                    if(token) {
+                        this.$store.commit('SAVE_CURRENTUSER', { ...user, token });
+                        this.$router.push(this.$route.query.redirect || '/feed/new');
+                    }
+                }).catch((err) => {
+                    console.log(err);
+                    this.btnLoading = false;
+                    const { response: { data = { message: '登录失败' } } = {} } = err;
+                    this.error = this.$MessageBundle(data);
+                });
+            } else {
+                return this.btnLoading = false;
+            }
         }
     },
-    mounted() {
-        // // todo
-        // window.app.style.height = '100%';
-        // this.$el.parentElement.style.height = '100%';
-    },
-    beforeDestroy() {
-        // window.app.style.height = '';
-        // this.$el.parentElement.style.height = '';
-    }
+    mounted() {},
+    beforeDestroy() {}
 }
 </script>
 <style lang='less'>
@@ -107,6 +140,12 @@ export default {
     }
     .normal_signin {
         flex: 1 1 auto;
+    }
+
+    .v-icon {
+        width: 38px;
+        height: 38px;
+        color: #ccc;
     }
 }
 
@@ -124,7 +163,8 @@ export default {
             vertical-align: middle;
             height: 0;
             width: 190px;
-            border-bottom: 1px solid currentColor; /*no*/
+            border-bottom: 1px solid currentColor;
+            /*no*/
         }
         ;
         &:before {
@@ -144,17 +184,14 @@ export default {
     &_icon {
         width: 80px;
         height: 80px;
+        line-height: 78px;
         border-radius: 50%;
         overflow: hidden;
         margin-bottom: 20px;
         box-shadow: 0 0 3px 3px #ededed;
         background: #fff;
-        position: relative;
-        svg {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
+
+        .v-icon {
             width: 40px;
             height: 40px;
         }
@@ -201,7 +238,8 @@ export default {
             flex: 0 0 auto;
         }
         &+& {
-            border-top: 1px solid #ededed; /*no*/
+            border-top: 1px solid #ededed;
+            /*no*/
         }
     }
 }
@@ -210,11 +248,13 @@ export default {
     padding: 30px;
     color: #f4504d;
     font-size: 24px;
+    height: 24*1.5+60px
 }
 
 .long_btn {
     display: block;
     box-sizing: border-box;
+    text-align: center;
     width: calc(~'100% - 60px');
     margin: 0 30px;
     height: 90px;
@@ -224,9 +264,21 @@ export default {
     color: #fff;
     border-radius: 12px;
     background: #59b6d7;
+    >div {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
     &[disabled] {
         background: rgb(211, 211, 211);
         cursor: not-allowed;
+    }
+    svg {
+        width: 32px;
+        height: 32px;
+        margin-left: 5px;
     }
 }
 
