6f0d67bb26d2be994065369f27c084a51f3d63ba
boxshadow <idafoo@sina.com>
2018-01-04 15:07:02 +0800

    chore: 增加排行榜公用组件
    1. 增加排行榜公用组件
    2. 更新用户排行榜页面框架

--- src/page/rank/children/users.vue
+++ src/page/rank/children/users.vue
@@ -1,11 +1,53 @@
 <template>
   <div>
-    用户
+    <rank-users-component v-for="rank in ranks" :title="rank.title" :api="rank.api" :listUrl="rank.listUrl" />
   </div>
 </template>
 
 <script>
+  import RankUsersComponent from '../components/rankUsersComponent'
+  const ranks = [
+    {
+      title: '全站粉丝排行榜',
+      listUrl: '/rank/users/followers',
+      api: 'ranks/followers'
+    },
+    {
+      title: '财富达人排行榜',
+      listUrl: '/rank/users/balance',
+      api: 'ranks/balance'
+    },
+    {
+      title: '收入达人排行榜',
+      listUrl: '/rank/users/income',
+      api: 'ranks/income'
+    },
+    {
+      title: '社区签到排行榜',
+      listUrl: '/rank/users/checkin',
+      api: 'checkin-ranks'
+    },
+    {
+      title: '社区专家排行榜',
+      listUrl: '/rank/users/question-experts',
+      api: 'question-ranks/experts'
+    },
+    {
+      title: '问答达人排行榜',
+      listUrl: '/rank/users/question-likes',
+      api: 'question-ranks/likes'
+    }
+  ]
+
   export default {
-    name: 'rankUsers'
+    name: 'rankUsers',
+    components: {
+      RankUsersComponent
+    },
+    data () {
+      return {
+        ranks
+      }
+    }
   }
 </script>
---
+++ src/page/rank/components/rankUsersComponent.vue
@@ -0,0 +1,79 @@
+<template>
+  <div :class="prefixCls">
+     <div :class="`${prefixCls}-title-label`">
+        <h6>{{ title }}</h6>
+        <div :class="`${prefixCls}-more`" @click="to(listUrl)">
+          <span>全部</span>
+          <v-icon type='base-arrow-r'></v-icon>
+        </div>
+     </div>
+  </div>
+</template>
+
+<script>
+  const prefixCls = 'rank-list-item'
+  export default {
+    name: 'rankUsersComponent',
+    props: {
+      api: {
+        required: true
+      },
+      listUrl: {
+        required: true
+      },
+      title: {
+        required: true
+      }
+    },
+
+    data () {
+      return {
+        prefixCls
+      }
+    },
+
+    methods: {
+      to(path) {
+        if (path) {
+          this.$router.push({ path })
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+  @rank-list-prefixCls: rank-list-item;
+
+  .@{rank-list-prefixCls} {
+    &-title-label {
+      font-size: 26px;
+      padding: 0 30px;
+      border-bottom: 1px solid #ededed;
+      /*no*/
+      height: 70px;
+      line-height: 68px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      h6 {
+        font-size: 30px;
+      }
+    }
+    &-more {
+      display: flex;
+      align-items: center;
+      span {
+        margin: 0 5px;
+        color: #999;
+      }
+      .v-icon {
+        width: 24px;
+        height: 24px;
+        transition: .5s ease;
+        font-size: 24px;
+        color: #999;
+      }
+    }
+  }
+</style>
