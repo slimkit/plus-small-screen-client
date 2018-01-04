5c1cf4afb9170c65c6a3cbbadbb7e102ce387517
boxshadow <idafoo@sina.com>
2018-01-04 16:22:03 +0800

    chore: 完善排行榜用户排行页面
    1. 完善排行榜用户排行页面
    2. 修改头像的border-radius值
    3. 完善公共组件

--- src/page/rank/children/users.vue
+++ src/page/rank/children/users.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <rank-users-component v-for="rank in ranks" :title="rank.title" :api="rank.api" :listUrl="rank.listUrl" />
+    <rank-users-component v-for="(rank, index) in ranks" :title="rank.title" :api="rank.api" :key="index" :listUrl="rank.listUrl" />
   </div>
 </template>
 
@@ -10,32 +10,32 @@
     {
       title: '全站粉丝排行榜',
       listUrl: '/rank/users/followers',
-      api: 'ranks/followers'
+      api: '/ranks/followers'
     },
     {
       title: '财富达人排行榜',
       listUrl: '/rank/users/balance',
-      api: 'ranks/balance'
+      api: '/ranks/balance'
     },
     {
       title: '收入达人排行榜',
       listUrl: '/rank/users/income',
-      api: 'ranks/income'
+      api: '/ranks/income'
     },
     {
       title: '社区签到排行榜',
       listUrl: '/rank/users/checkin',
-      api: 'checkin-ranks'
+      api: '/checkin-ranks'
     },
     {
       title: '社区专家排行榜',
       listUrl: '/rank/users/question-experts',
-      api: 'question-ranks/experts'
+      api: '/question-ranks/experts'
     },
     {
       title: '问答达人排行榜',
       listUrl: '/rank/users/question-likes',
-      api: 'question-ranks/likes'
+      api: '/question-ranks/likes'
     }
   ]
 
--- src/page/rank/components/rankUsersComponent.vue
+++ src/page/rank/components/rankUsersComponent.vue
@@ -1,12 +1,18 @@
 <template>
-  <div :class="prefixCls">
-     <div :class="`${prefixCls}-title-label`">
-        <h6>{{ title }}</h6>
-        <div :class="`${prefixCls}-more`" @click="to(listUrl)">
-          <span>全部</span>
-          <v-icon type='base-arrow-r'></v-icon>
-        </div>
-     </div>
+  <div v-if="show" :class="prefixCls">
+    <div :class="`${prefixCls}-label`">
+      <h6>{{ title }}</h6>
+      <div :class="`${prefixCls}-more`" @click="to(listUrl)">
+        <span>全部</span>
+        <v-icon type='base-arrow-r'></v-icon>
+      </div>
+    </div>
+    <div :class="`${prefixCls}-label`">
+      <div :class="`${prefixCls}-user`" v-for="user in getShow" :key="user.id">
+        <v-avatar :class="`${prefixCls}-user-avatar`" :src='user.avatar' :sex='user.sex'></v-avatar>
+        <p class="ellipsis">{{ user.name }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,7 +34,31 @@
 
     data () {
       return {
-        prefixCls
+        prefixCls,
+        users: []
+      }
+    },
+
+    computed: {
+      /**
+       * 是否展示该排行模块
+       * @Author   Wayne
+       * @DateTime 2018-01-04
+       * @Email    qiaobin@zhiyicx.com
+       * @return   Boolean
+       */
+      show () {
+        return this.users.length > 0
+      },
+      /**
+       * 获取前个要被展示的用户
+       * @Author   Wayne
+       * @DateTime 2018-01-04
+       * @Email    qiaobin@zhiyicx.com
+       * @return   Array
+       */
+      getShow() {
+        return this.users.slice(0, 5)
       }
     },
 
@@ -37,7 +67,26 @@
         if (path) {
           this.$router.push({ path })
         }
+      },
+
+      getUsers() {
+        this.$http.get(
+          this.api,
+          {
+            validateStatus: status => status === 200
+          }
+        )
+        .then(({ data = [] }) => {
+          this.users = [ ...data ]
+        })
+        .catch(({ response: { data } }) => {
+          console.log(data)
+        })
       }
+    },
+
+    beforeMount() {
+      this.getUsers()
     }
   }
 </script>
@@ -46,11 +95,15 @@
   @rank-list-prefixCls: rank-list-item;
 
   .@{rank-list-prefixCls} {
-    &-title-label {
+    &-label {
+      &:nth-child(2) {
+        height: 194px;
+        border-bottom: 8px solid #f4f5f5;
+        margin-left: -30px;
+        justify-content: flex-start;
+      }
       font-size: 26px;
       padding: 0 30px;
-      border-bottom: 1px solid #ededed;
-      /*no*/
       height: 70px;
       line-height: 68px;
       display: flex;
@@ -60,12 +113,34 @@
         font-size: 30px;
       }
     }
+    &-user {
+      font-size: 26px;
+      width: calc(~'20% - ' 30px);
+      margin-left: 30px;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      text-align: center;
+      &-avatar {
+        width: 100px;
+        height: 100px;
+        span {
+          color: #666;
+          width: 100%;
+        }
+        img {
+          width: 100px;
+          height: 100px;
+        }
+      }
+    }
     &-more {
       display: flex;
       align-items: center;
       span {
         margin: 0 5px;
         color: #999;
+        font-size: 24px;
       }
       .v-icon {
         width: 24px;
