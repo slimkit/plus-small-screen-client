8dc2197d6f508581bc18d97c5ab2a950d550e1c2
JsonLeex <jsonleex@163.com>
2018-02-12 20:54:12 +0800

    chore: Fix Bug of 'find'

--- src/page/find/children/findContent.vue
+++ src/page/find/children/findContent.vue
@@ -12,69 +12,72 @@
     </div>
 </template>
 <script>
-import findItem from '../components/findItem'
+import findItem from '../components/findItem';
 export default {
   name: 'findContent',
   components: {
     findItem
   },
-  data () {
+  data() {
     return {
       user_list: [],
       loading: true
-    }
+    };
   },
 
   computed: {
-    type () {
-      return this.$route.meta.type
+    type() {
+      return this.$route.meta.type;
     },
-    noData () {
-      return this.user_list.length === 0 && !this.loading
+    noData() {
+      return this.user_list.length === 0 && !this.loading;
     }
   },
 
   /**
-     * 判断是否刷新数据
-     * @author jsonleex <jsonlseex@163.com>
-     * @param  {Object}                 to
-     * @param  {Object}                 from
-     * @param  {Function}               next
-     */
-  beforeRouteEnter (to, from, next) {
+   * 判断是否刷新数据
+   * @author jsonleex <jsonlseex@163.com>
+   * @param  {Object}                 to
+   * @param  {Object}                 from
+   * @param  {Function}               next
+   */
+  beforeRouteEnter(to, from, next) {
     next(vm => {
       if (to.meta.type !== from.meta.type) {
-        vm.getUsers()
+        vm.getUsers();
       }
-    })
+    });
   },
   methods: {
-    getUsers () {
-      this.loading = true
-      this.$store.dispatch('GET_USER_DATA', this.type, {
-        limt: 15
-      }).then(({ data }) => {
-        this.loading = false
-        if (data && data.length > 0) {
-          this.user_list = data
-        }
-      }).catch(err => {
-        console.log(err)
-      })
+    getUsers() {
+      this.loading = true;
+      this.$store
+        .dispatch('GET_USER_DATA', {
+          type: this.type,
+          limt: 15
+        })
+        .then(({ data }) => {
+          this.loading = false;
+          if (data && data.length > 0) {
+            this.user_list = data;
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
-}
+};
 </script>
 <style lang='less'>
-    
-    .full{
-        display: block;
-        position: fixed;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        background-color: #fff;
-        z-index: 99
-    }
+.full {
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: #fff;
+  z-index: 99;
+}
 </style>
--- src/stores/module/user/actions.js
+++ src/stores/module/user/actions.js
@@ -1,77 +1,78 @@
-import http from '@/http'
+import http from '@/http';
 export default {
-  GET_USER_DATA({ commit }, {
-    type,
-    limit = 15,
-    offset = 0
-  }) {
-    let uri
-
+  GET_USER_DATA({ commit }, { type, limit = 15, offset = 0 }) {
+    let uri;
+    console.log(type);
     switch (type) {
       case 'pop':
-        uri = '/user/populars'
-        break
+        uri = '/user/populars';
+        break;
       case 'new':
-        uri = '/user/latests'
-        break
+        uri = '/user/latests';
+        break;
       case 'rec':
-        uri = '/user/find-by-tags'
-        break
+        uri = '/user/find-by-tags';
+        break;
     }
     return new Promise((resolve, reject) => {
       let res = {
         noMore: false,
         data: []
-      }
-      http.get(`${uri}?limit=${limit}&offset=${offset}`).then(({ data = [] }) => {
-        if (data.length > 0) {
-          res.data = data
-          res.noMore = data.length < limit
-          commit('SAVE_USER', data)
-        }
-        resolve(res)
-      }).catch(err => {
-        reject(err)
-      })
-    })
+      };
+      http
+        .get(`${uri}?limit=${limit}&offset=${offset}`)
+        .then(({ data = [] }) => {
+          if (data.length > 0) {
+            res.data = data;
+            res.noMore = data.length < limit;
+            commit('SAVE_USER', data);
+          }
+          resolve(res);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
   },
 
   FOLLOW_USER({ commit }, { id, status }) {
     // PUT /user/followings/:user
     // DELETE /user/followings/:user
-    let method
+    let method;
 
     switch (status) {
       case 'unFollow':
-        method = 'PUT'
-        break
+        method = 'PUT';
+        break;
       case 'eachFollow':
-        method = 'DELETE'
-        break
+        method = 'DELETE';
+        break;
       case 'follow':
-        method = 'DELETE'
-        break
+        method = 'DELETE';
+        break;
     }
 
-    if (!method) return false
+    if (!method) return false;
 
-    let url = `/user/followings/${id}`
+    let url = `/user/followings/${id}`;
 
     const res = {
       status: true,
       follower: method === 'PUT'
-    }
+    };
 
     return new Promise((resolve, reject) => {
       http({
         method,
         url,
         validateStatus: s => s === 204
-      }).then(() => {
-        resolve(res)
-      }).catch(err => {
-        reject(err)
       })
-    })
+        .then(() => {
+          resolve(res);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
   }
-}
+};
