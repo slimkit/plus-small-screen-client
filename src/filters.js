f9e3f436ce69700829b6aba7915a3f371a2bdee0
JsonLeex <jsonleex@163.com>
2018-02-12 20:12:01 +0800

    chore: Modify 'filters.js'

--- src/filters.js
+++ src/filters.js
@@ -1,3 +1,4 @@
+import timeago from 'timeago.js';
 import plueMessageBundle from 'plus-message-bundle';
 
 /**
@@ -9,6 +10,18 @@ import plueMessageBundle from 'plus-message-bundle';
  * @author Seven Du <shiweidu@outlook.com>
  */
 export function plusMessageFirst(message, defaultMessage) {
+  return plueMessageBundle(message, defaultMessage).getMessage();
+}
+
+/**
+ * ThinkSNS Plus 消息解析器，获取顶部消息.
+ *
+ * @param {Object} message
+ * @param {String} defaultMessage
+ * @return {String}
+ * @author Seven Du <shiweidu@outlook.com>
+ */
+export function plusMessageAnalyze(message, defaultMessage) {
   return plueMessageBundle(message, defaultMessage).getMessage();
 }
 
@@ -63,38 +76,14 @@ export const UTC2localTime = UTCDateString => {
   return dateStr;
 };
 
-/**
- * 格式化 时间提示
- * @author Jsonleex <jsonlseex@163.com>
- * @param  {String}
- * @return {String}
- */
-export const time2tips = str => {
-  if (!str) return '';
+// 时差
+const timeOffset = new Date().getTimezoneOffset() * 60 * 1000;
 
-  // 兼容 IOS 保证传入数据格式 YYYY/MM/dd HH:mm:ss
-  let date = new Date(str.replace(/-/g, '/'));
-
-  // 现在的时间-传入的时间 = 相差的时间（单位 = 毫秒）
-  let time = new Date().getTime() - date.getTime();
-
-  if (time < 0) {
-    return '';
-  } else if (time / 1000 < 30) {
-    return '刚刚';
-  } else if (time / 1000 < 60) {
-    return parseInt(time / 1000) + '秒前';
-  } else if (time / 60000 < 60) {
-    return parseInt(time / 60000) + '分钟前';
-  } else if (time / 3600000 < 24) {
-    return parseInt(time / 3600000) + '小时前';
-  } else if (time / 86400000 < 31) {
-    return parseInt(time / 86400000) + '天前';
-  } else if (time / 2592000000 < 12) {
-    return parseInt(time / 2592000000) + '月前';
-  } else {
-    return parseInt(time / 31536000000) + '年前';
-  }
+export const dateFormat = timeago(null, 'zh_CN');
+export const time2tips = date => {
+  return dateFormat.format(
+    new Date(date.replace(/-/g, '/')).getTime() - timeOffset
+  );
 };
 
 /**
