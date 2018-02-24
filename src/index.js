805291b6972cc59d9e8f911a77b1bafce3504ea0
JsonLeex <jsonleex@163.com>
2018-02-24 18:24:31 +0800

    chore: Split  components to 'components.js'

--- src/index.js
+++ src/index.js
@@ -2,49 +2,28 @@ import Vue from 'vue';
 import store from './stores/';
 import router from './routers/';
 import directives from './directives/';
+
+import './util/rem';
+import _ from 'lodash';
+
 import Modal from './plugins/modal/';
 import imgLazy from './plugins/imgLazy';
 import Message from './plugins/message/';
 import imgCropper from './plugins/imgCropper';
-import _ from 'lodash';
-import './util/rem';
+
+import './style/index.less';
 import 'github-markdown-css';
 
-/* h5 调试 */
-import './style/index.less';
 // Filters
 import * as filters from './filters';
 /** Ajax */
 import Ajax from './http';
 
-/** common components */
-import vIcon from './components/common/vIcons'; /* Icons */
-import btnSwitch from './components/common/btnSwitch'; /* btnSwitch 按钮 */
-import badge from './components/common/badge'; /* Badge 徽标 */
-import fullSpin from './components/FullSpin'; /* 全屏加载动画 */
-import userAvatar from './components/VAvatar'; /* Avatar 用户头像组件 */
-import loadMore from './components/loadMore';
-import HeadTop from './components/HeadTop';
-import FootGuide from './components/FootGuide';
-import DiySelect from './components/DiySelect';
-
-const components = [
-  vIcon,
-  btnSwitch,
-  badge,
-  userAvatar,
-  loadMore,
-  fullSpin,
-  HeadTop,
-  FootGuide,
-  DiySelect
-];
+import components from './components.js';
 
 components.forEach(component => {
   Vue.component(component.name, component);
 });
-
-Vue.config.productionTip = false;
 
 Vue.prototype.$http = Ajax; /* Ajax */
 Vue.prototype.$Modal = Modal;
@@ -53,11 +32,13 @@ Vue.prototype.$MessageBundle = filters.plusMessageFirst;
 
 Vue.use(imgLazy); /* 图片懒加载 */
 Vue.use(imgCropper); /* 图片裁剪 */
+
 Vue.config.productionTip = false;
 
 for (const k in filters) {
   Vue.filter(k, filters[k]);
 }
+
 // 自定义指令
 for (const k in directives) {
   Vue.directive(k, directives[k]);
