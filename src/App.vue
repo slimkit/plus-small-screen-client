a0813409b0764befacba30a2a3a7d042443f8a3a
JsonLeex <jsonleex@163.com>
2018-03-07 16:14:14 +0800

    feat: Add component "pswp"

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,28 @@
     </keep-alive>
     <router-view v-if="!$route.meta.keepAlive"></router-view>
     <svg-icon></svg-icon>
+    <pswp></pswp>
   </div>
 </template>
 <script>
+import pswp from '@/components/pswp.vue';
 export default {
   name: 'app',
   components: {
-    svgIcon: () => import(/* webpackChunkName: 'svgIcon' */ '@/components/common/svgIcon')
+    pswp,
+    svgIcon: () =>
+      import(/* webpackChunkName: 'svgIcon' */ '@/components/common/svgIcon')
   }
-}
-
+};
 </script>
 <style lang="less">
-.router-fade-enter-active, .router-fade-leave-active {
-  transition: opacity .3s;
+.router-fade-enter-active,
+.router-fade-leave-active {
+  transition: opacity 0.3s;
 }
 
-.router-fade-enter, .router-fade-leave-active {
+.router-fade-enter,
+.router-fade-leave-active {
   opacity: 0;
 }
-
 </style>
