0a3931cff1c31f0881d3ab91a84965a44b28570a
JsonLeex <jsonleex@163.com>
2018-03-12 11:13:31 +0800

    chore: 修改配置文件
    1. 移除"--open"参数
    2. 新增环境变量"VUE_APP_THEME_PATH"
    3. 新增方法"getLessVariables"

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
+const fs = require("fs");
+function getLessVariables(file) {
+  const themeContent = fs.readFileSync(file, "utf-8");
+  const variables = {};
+  themeContent.split("\n").forEach(item => {
+    if (item.indexOf("//") > -1 || item.indexOf("/*") > -1) {
+      return;
+    }
+    const _pair = item.split(":");
+    if (_pair.length < 2) return;
+    const key = _pair[0].replace("\r", "").replace("@", "");
+    if (!key) return;
+    const value = _pair[1]
+      .replace(";", "")
+      .replace("\r", "")
+      .replace(/^\s+|\s+$/g, "");
+    variables[key] = value;
+  });
+  return variables;
+}
 module.exports = {
   baseUrl: process.env.BASE_URL || "/",
   lintOnSave: true,
@@ -6,11 +26,12 @@ module.exports = {
     sourceMap: !!eval(process.env.GENERATE_CSS_MAP),
     loaderOptions: {
       less: {
-        globalVars: require("./theme")
+        globalVars: getLessVariables(process.env.VUE_APP_THEME_PATH)
       }
     }
   },
   devServer: {
+    open: false,
     proxy: {
       "/api": {
         target: process.env.VUE_APP_API_HOST,
