f9c3ae54acdf4460aef7bba95ff7ef5de2be206e
Seven Du <shiweidu@outlook.com>
2018-04-08 19:02:27 +0800

    docs: 增加环信 APP KEY 设置

--- src/easemob/webim.config.js
+++ src/easemob/webim.config.js
@@ -1,7 +1,7 @@
 export default {
   xmppURL: "im-api.easemob.com", // xmpp Server地址，对于在console.easemob.com创建的appKey，固定为该值
   apiURL: "http://a1.easemob.com", // rest Server地址，对于在console.easemob.com创建的appkey，固定为该值
-  appkey: "#", // App key
+  appkey: process.env.VUE_APP_EASEMOB_APP_KEY, // App key
   https: false, // 是否使用https
   isMultiLoginSessions: false, // 是否开启多页面同步收消息，注意，需要先联系商务开通此功能
   isAutoLogin: true, // 自动出席，（如设置为false，则表示离线，无法收消息，需要在登录成功后手动调用conn.setPresence()才可以收消息）
