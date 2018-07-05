cb8734b66682479f5c33a2db5a6de5da094dbc3e
mutoe <mutoe@foxmail.com>
2018-07-05 17:19:45 +0800

    style: code format

--- src/page/message/list/comments.vue
+++ src/page/message/list/comments.vue
@@ -1,14 +1,14 @@
 <template>
   <div :class="`${prefixCls}`">
-    <header 
-      slot="head" 
+    <header
+      slot="head"
       class="m-box m-justify-bet m-aln-center m-head-top m-pos-f m-main m-bb1">
       <div class="m-box m-flex-grow1 m-aln-center m-flex-base0">
-        <svg 
-          class="m-style-svg m-svg-def" 
+        <svg
+          class="m-style-svg m-svg-def"
           @click="goBack">
-          <use 
-            xmlns:xlink="http://www.w3.org/1999/xlink" 
+          <use
+            xmlns:xlink="http://www.w3.org/1999/xlink"
             xlink:href="#base-back"/>
         </svg>
       </div>
@@ -17,20 +17,22 @@
       </div>
       <div class="m-box m-flex-grow1 m-aln-center m-flex-base0 m-justify-end"/>
     </header>
-    <div 
-      :class="`${prefixCls}-container`" 
+    <div
+      :class="`${prefixCls}-container`"
       style="padding-top: 0.9rem">
       <load-more
         ref="loadmore"
         :on-refresh="onRefresh"
         :on-load-more="onLoadMore"
         :class="`${prefixCls}-loadmore`">
-        <div 
-          v-for="comment in comments" 
-          :class="`${prefixCls}-item`" 
-          :key="`comment-key-${comment.id}`"><component 
-            :is="items[comment.commentable_type]" 
-            :comment="comment"/></div>
+        <div
+          v-for="comment in comments"
+          :class="`${prefixCls}-item`"
+          :key="`comment-key-${comment.id}`">
+          <component
+            :is="items[comment.commentable_type]"
+            :comment="comment"/>
+        </div>
       </load-more>
     </div>
   </div>
