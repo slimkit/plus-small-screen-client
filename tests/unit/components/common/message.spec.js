245b090cc5699c28241aea97ff802948f798d398
Seven Du <shiweidu@outlook.com>
2018-07-09 15:34:17 +0800

    test(component/common/message): Add a common message component unit test

--- tests/unit/components/common/message.spec.js
+++ tests/unit/components/common/message.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, mount } from "@vue/test-utils";
 import Message from "@/components/common/message.vue";
 
 describe("@/components/common/message.vue", () => {
@@ -16,6 +16,30 @@ describe("@/components/common/message.vue", () => {
 
       expect(wrapper.classes()).toContain(className);
     });
+  });
+
+  it("Renders alert close DOM", () => {
+    const wrapper = mount(Message);
+
+    expect(wrapper.find('.Alert__close').exists()).toBe(true);
+  });
+
+  it("Renders default slot when passed", () => {
+    const message = "Default! Hello World~";
+    const wrapper = shallowMount(Message);
+
+    expect(wrapper.text()).toMatch(message);
+  });
+
+  it("Renders default slot", () => {
+    const message = "Hi, I'm is a message.🎉";
+    const wrapper = shallowMount(Message, {
+      slots: {
+        default: message
+      }
+    });
+
+    expect(wrapper.text()).toMatch(message);
   });
 });
 
