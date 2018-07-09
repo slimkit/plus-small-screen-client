60dbb19fcb30df31f8dc50a9796cf9372694dd43
Seven Du <shiweidu@outlook.com>
2018-07-09 15:38:53 +0800

    test(*): Formatting

--- tests/unit/components/common/message.spec.js
+++ tests/unit/components/common/message.spec.js
@@ -2,13 +2,13 @@ import { shallowMount, mount } from "@vue/test-utils";
 import Message from "@/components/common/message.vue";
 
 describe("@/components/common/message.vue", () => {
-  it('Check props type when passed', () => {
+  it("Check props type when passed", () => {
     const types = [
       { type: "success", className: "Alert--Success" },
       { type: "warning", className: "Alert--Warning" },
       { type: "error", className: "Alert--Error" }
     ];
-    
+
     types.map(({ type, className }) => {
       let wrapper = shallowMount(Message, {
         propsData: { type }
@@ -21,7 +21,7 @@ describe("@/components/common/message.vue", () => {
   it("Renders alert close DOM", () => {
     const wrapper = mount(Message);
 
-    expect(wrapper.find('.Alert__close').exists()).toBe(true);
+    expect(wrapper.find(".Alert__close").exists()).toBe(true);
   });
 
   it("Renders default slot when passed", () => {
@@ -42,4 +42,3 @@ describe("@/components/common/message.vue", () => {
     expect(wrapper.text()).toMatch(message);
   });
 });
-
