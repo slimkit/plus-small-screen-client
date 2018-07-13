163681af7eb8e4244311704cbdb573e97487fc44
Seven Du <shiweidu@outlook.com>
2018-07-13 20:51:38 +0800

    refactor(api): Refactor bootstrapers request APIs

--- src/api/bootstrappers.js
+++ src/api/bootstrappers.js
@@ -3,49 +3,34 @@ import lstore from "@/plugins/lstore";
 import location from "@/util/location.js";
 /**
  * 获取热门城市列表
- * @author jsonleex <jsonlseex@163.com>
- * @return {Promise -> Array}
+ * @return {Promise}
  */
-export function getHotCities() {
-  const hasData = lstore.hasData("H5_HOT_CITIES");
-  return hasData
-    ? Promise.resolve(lstore.getData("H5_HOT_CITIES"))
-    : api.get("/locations/hots").then(
-        ({ data }) => {
-          const res = data ? data : [];
-          lstore.setData("H5_HOT_CITIES", res);
-          return res;
-        },
-        err => {
-          console.warn(err);
-          return [];
-        }
-      );
+export async function getHotCities() {
+  if (lstore.hasData("H5_HOT_CITIES")) {
+    return lstore.getData("H5_HOT_CITIES");
+  }
+
+  let response = await api.get("/locations/hots", {
+    validateStatus: status => status === 200
+  });
+  lstore.setData("H5_HOT_CITIES", response.data);
+
+  return response.data;
 }
 
 /**
  * 获取当前定位信息
- * @author jsonleex <jsonlseex@163.com>
- * @return {Promise -> Object}
+ * @return {Promise}
  */
-export function getCurrentPosition() {
-  return location.getCurrentPosition().then(
-    data => {
-      //   str.push("经度：" + position.getLng());
-      //   str.push("纬度：" + position.getLat());
-      const { addressComponent: { city, district, street = "" } = {} } = data;
-      const res = {
-        lng: data.position.getLng(),
-        lat: data.position.getLat(),
-        label: street || district || city || "定位成功"
-      };
-      return res;
-    },
-    err => {
-      console.warn(err);
-      throw err;
-    }
-  );
+export async function getCurrentPosition() {
+  let data = await location.getCurrentPosition();
+  let { city, district, street } = data.addressComponent || {};
+
+  return {
+    lng: data.position.getLng(),
+    lat: data.position.getLat(),
+    label: street || district || city || "定位成功"
+  };
 }
 
 export function getGeo(address) {
@@ -72,13 +57,21 @@ export function getGeo(address) {
 
 /**
  * 搜索城市
- * @author jsonleex <jsonlseex@163.com>
  * @param  {String} name
  * @return {Promise -> Array}
  */
-export function searchCityByName(name) {
-  if (!name) return Promise.resolve({ data: [] });
-  return api.get(`/locations/search?name=${name}`);
+export async function searchCityByName(
+  name,
+  defaultResponseValue = { data: [] }
+) {
+  if (!name) {
+    return defaultResponseValue;
+  }
+
+  return await api.get("/locations/search", {
+    params: { name },
+    validateStatus: status => status === 200
+  });
 }
 
 /**
@@ -103,15 +96,18 @@ export function getBootstrappers() {
 
 /**
  * 根据广告位 id 获取一个广告位的广告列表
- * @author mutoe <mutoe@foxmail.com>
  * @export
  * @param {number} adId 从广告类型列表中获取的广告 id
  * @returns
  */
-export function getAdsById(adId) {
-  if (!adId) return Promise.resolve({ data: [] });
-  const url = `/advertisingspace/${adId}/advertising`;
-  return api.get(url, { validateStatus: s => s === 200 });
+export async function getAdsById(adId, defaultResponseValue = { data: [] }) {
+  if (!adId) {
+    return defaultResponseValue;
+  }
+
+  return await api.get(`/advertisingspace/${adId}/advertising`, {
+    validateStatus: status => status === 200
+  });
 }
 
 /**
